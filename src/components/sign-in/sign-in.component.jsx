922b3eede1524d93340ce8c1e13aeb867eea5524
Usman <Murtala>
2019-11-14 22:28:46 +0100

    Created a shop page, a header component and a sign in component

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import FormInput from '../form-input/form-input.component'
+import CustomButton from '../custom-button/custom-button.component'
+import './sign-in.styles.scss'
+
+class SignIn extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.setState({email: '', password: ''})
+    }
+
+    handleChange = event => {
+        const {value, name} = event.target;
+        this.setState( { [name]: value } );
+    }
+
+    render(){
+
+        const {email, password} = this.state;
+
+        return(
+            <div className='sign-in'>
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput 
+                        type="email"
+                        name="email" 
+                        value={email} 
+                        label="Email"
+                        required 
+                        handleChange={this.handleChange}/>
+                    <FormInput 
+                        type="password" 
+                        name="password" 
+                        value={password}
+                        label="Password" 
+                        required 
+                        handleChange={this.handleChange}/>
+                    <CustomButton type="submit"> Sign in </CustomButton>
+                </form>
+
+            </div>
+        )
+    }
+}
+
+export default SignIn;
